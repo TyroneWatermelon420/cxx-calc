1f9d0671b4d44195ffb69df3d2eb089308ef4daa
Stormx920 <eriklrosebrock@tutanota.com>
2023-05-02 16:42:21 +0200

    really starting

---
+++ build/CMakeFiles/cxx-test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/erik/Dokumente/Code/Git/cxx-test/bin/cxx-test"
+  "/home/erik/Dokumente/Code/Git/cxx-test/bin/cxx-test.pdb"
+  "CMakeFiles/cxx-test.dir/src/main.cpp.o"
+  "CMakeFiles/cxx-test.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cxx-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cxx-test.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cxx-test.
+# This may be replaced when dependencies are built.
