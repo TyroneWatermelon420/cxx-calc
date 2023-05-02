175797bac9621f06461462c7ce5617bf887a0596
Stormx920 <eriklrosebrock@tutanota.com>
2023-05-02 22:09:19 +0200

    some more updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+int number1;
+int number2;
+
 int main(){
-    std::cout << "hello" << std::endl;
+    std::cout << "Welcome to a Simple Calculator!\nEnter two numbers:" << std::endl;
+    std::cin >> number1;
+    std::cin >> number2;
+    std::cout << number1 << number2 << std::endl;
 }
