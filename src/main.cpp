d3281d7df1ffbee136f43e4734f006728fef6da4
Stormx920 <eriklrosebrock@tutanota.com>
2023-07-30 23:55:21 +0200

    finisch calculator

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
-#include<cmath>
+
+using namespace std;
 
 double number1;
 double number2;
@@ -7,18 +8,30 @@ double number2;
 char operation;
 
 int main(){
-    std::cout << "********************************************\n\n\nWelcome to the simple calculator!\nEnter the operation you want to use +,-,*,/\n-->  ";
-    std::cin >> operation;
-    std::cout << std::endl << std::endl;
-    std::cout << "Enter two numbers for your calculation\nFirst number\n-->  ";
-    std::cin >> number1;
-    std::cout << "Second number\n-->  ";
-    std::cin >> number2;
+    cout << "********************************************\n\n\nWelcome to the simple calculator!\nEnter the operation you want to use +,-,*,/\n-->  ";
+    cin >> operation;
+    cout << endl << endl;
+    cout << "Enter two numbers for your calculation\nFirst number\n-->  ";
+    cin >> number1;
+    cout << "Second number\n-->  ";
+    cin >> number2;
 
     switch (operation) {
         case'+':
-            std::cout << number1 << "+" << number2 << " is ";
-            std::cout << number1 + number2 << std::endl;
+            cout << number1 << "+" << number2 << " is ";
+            cout << number1 + number2 << endl;
             break;
+        case'-':
+            cout << number1 << "-" << number2 << " is ";
+            cout << number1 - number2 << endl;
+            break;
+        case'*':
+            cout << number1 << "*" << number2 << " is ";
+            cout << number1 * number2 << endl;
+            break;
+        case'/':
+            cout << number1 << "/" << number2 << " is ";
+            cout << number1 / number2 << endl;
+        break;
     }
 }
